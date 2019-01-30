2019477507cea3d174b090f9a7e08c4bee48b779
Henrique Morin Naimaier <coubron@yahoo.com.br>
2019-01-30 11:27:11 -0200

    Adicionado campo preco no filme, sala e sessao

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -1,9 +1,11 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.Duration;
 
 /**
  * Created by nando on 03/03/17.
@@ -17,20 +19,22 @@ public class Filme {
     private String nome;
     private Duration duracao;
     private String genero;
+    private BigDecimal preco;
     
     
-    
-    /**
+
+	/**
      * @deprecated hibernate only
      */
     public Filme() {
 
     }
     
-    public Filme(String nome, Duration duracao, String genero) {
+    public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
 		this.nome = nome;
 		this.duracao = duracao;
 		this.genero = genero;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -63,5 +67,13 @@ public class Filme {
 
     public void setGenero(String genero) {
         this.genero = genero;
+    }
+    
+    public BigDecimal getPreco() {
+    	return preco;
+    }
+    
+    public void setPreco(BigDecimal preco) {
+    	this.preco = preco;
     }
 }
--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.validacao;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -17,10 +18,10 @@ public class GerenciadorDeSessaoTest {
 	@Test
 	public void garanteQueNaoDevePermitirSessaoNoMesmoHorario() {
 		
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI");
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
 		LocalTime horario = LocalTime.parse("10:00:00");
 		
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
 		
 		Sessao sessao = new Sessao(horario, filme, sala);
 		
@@ -33,10 +34,10 @@ public class GerenciadorDeSessaoTest {
 	
 	@Test
 	public void garanteQueNaoDevePermitirSessoesTerminandoDentroDoHorarioDeUmaSessaoJaExistente() {
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI");
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
 		LocalTime horario = LocalTime.parse("10:00:00");
 		
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Elsorado - IMAX", BigDecimal.ONE);
 		List<Sessao> sessoes = Arrays.asList(new Sessao(horario, filme, sala));
 		
 		Sessao sessao = new Sessao(horario.minusHours(1), filme, sala);
@@ -47,9 +48,9 @@ public class GerenciadorDeSessaoTest {
 	
 	@Test
 	public void garanteQueNaoDevePermitirSessoesIniciandoDentroDoHorarioDeUmaSessaoJaExistente() {
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI");
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
 		LocalTime horario = LocalTime.parse("10:00:00");
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
 		
 		List<Sessao> sessoesDaSala = Arrays.asList(new Sessao(horario, filme, sala));
 		
@@ -62,13 +63,13 @@ public class GerenciadorDeSessaoTest {
 	
 	@Test
 	public void garanteQueDevePermitirUmaInsercaoEntreDoisFilmes() {
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
 		
-		Filme filme1 = new Filme("Rogue One", Duration.ofMinutes(90), "SCI-FI");
+		Filme filme1 = new Filme("Rogue One", Duration.ofMinutes(90), "SCI-FI", BigDecimal.ONE);
 		LocalTime dezHoras = LocalTime.parse("10:00:00");
 		Sessao sessaoDasDez = new Sessao(dezHoras, filme1, sala);
 		
-		Filme filme2 = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI");
+		Filme filme2 = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
 		LocalTime dezoitoHoras = LocalTime.parse("18:00:00");
 		Sessao sessaoDasDezoito = new Sessao(dezoitoHoras, filme2, sala);
 		
@@ -79,5 +80,18 @@ public class GerenciadorDeSessaoTest {
 		Sessao sessao = new Sessao(LocalTime.parse("13:00:00"), filme1, sala);
 		
 		Assert.assertTrue(gerenciador.cabe(sessao));
+	}
+	
+	@Test
+	public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisOPrecoDoFilme() {
+		
+		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("22.5"));
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12.0"));
+		
+		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
+		
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		
+		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
 	}
 }
